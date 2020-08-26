cf21c9f39bee825052110861bc892cd84e87d1b4
zfy-zhang <zfy_zang@163.com>
2020-08-26 21:00:31 +0800

    no message

---
+++ spring-cloud-eureka-client/src/main/java/com/pat/controller/StatusController.java
@@ -0,0 +1,20 @@
+package com.pat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 状态控制器
+ * @Author <a href="mailto:zfy_zang@163.com">Vincent</a>
+ * @Create 2020/8/26
+ * @Modify
+ * @since
+ */
+@RestController("/actuator")
+public class StatusController {
+
+        @GetMapping("/status")
+    public String status() {
+        return "OK";
+    }
+}
